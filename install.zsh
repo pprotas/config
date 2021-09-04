04dc7888de59eef939371188d281863caf25ca15
Pawel Protas <pawprotas@gmail.com>
2021-09-04 17:21:05 +0200

    Add install script

---
+++ install.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+cd $HOME
+
+# Xcode CLT installation
+xcode-select --install
+
+# Homebrew installation
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Oh My Zsh installation
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Starship prompt installation
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+
